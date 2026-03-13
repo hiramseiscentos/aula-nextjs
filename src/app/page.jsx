fb355355d7727b09f3f84fea6eaaf98330dfc3eb
hiramseiscentos <hiramseiscentos@gmail.com>
2026-03-13 13:40:58 -0300

    aaa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
-import image from "next/image";
-import styles from "./page.module.css"
+import Link from "next/link";
+import styles from "./page.module.css";
 
 function home () {
     return (
         <div className={styles.containerHome}>
         <div className={styles.containerListas}>
             <h1>exemplos</h1>
+            <Link href="/exemplos/ex-01">exemplo 1</Link>
+            <Link href="/exemplos/ex-02">exemplo 2</Link>
         </div>
 
         <div className={styles.containerListas}>
